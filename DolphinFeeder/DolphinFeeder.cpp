6b8cb94237b83b1083a31e3119c2ea302d9ad856
Ben Drees <50091002+ben1894@users.noreply.github.com>
2021-03-26 16:32:05 -0400

    File Update
    Update SDK and lib to 64x

--- DolphinFeeder/DolphinFeeder.cpp
+++ DolphinFeeder/DolphinFeeder.cpp
@@ -15,12 +15,16 @@ int main()
 	if (!vJoyEnabled())
 	{
 		_tprintf(_T("Failed Getting vJoy attributes.\n"));
+		Sleep(1000);
 		return -2;
-	}	WORD VerDll, VerDrv;
+	}
+
+	WORD VerDll, VerDrv;
 	if (!DriverMatch(&VerDll, &VerDrv))
 		_tprintf(_T("Failed\r\nvJoy Driver (version %04x) does not match vJoyInterface DLL(version % 04x)\n"), VerDrv ,VerDll);
 	else
-			_tprintf(_T("OK - vJoy Driver and vJoyInterface DLL match vJoyInterface DLL (version % 04x)\n"), VerDrv);	
+			_tprintf(_T("OK - vJoy Driver and vJoyInterface DLL match vJoyInterface DLL (version % 04x)\n"), VerDrv);
+	
 	int iInterface = 1;
 	VjdStat status = GetVJDStatus(iInterface);
 
@@ -128,14 +132,3 @@ int main()
 		Sleep(10);
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
